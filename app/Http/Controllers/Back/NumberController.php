3ae4a710bcc8a73acbd47e051c684686cd87a236
anarceferov <anarceferov1996@gmail.com>
2021-08-07 14:44:27 +0400

    excel

--- app/Http/Controllers/Back/NumberController.php
+++ app/Http/Controllers/Back/NumberController.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace App\Http\Controllers\Back;
+
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\NumbersImport;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ImportRequest;
 use App\Http\Requests\NumberCreate;
 use App\Http\Requests\NumberUpdate;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Models\Number;
 
@@ -17,7 +21,7 @@ class NumberController extends Controller
 
         $numbers = Number::orderByDesc('id')->paginate(20);
 
-        return view('back.number.list' , compact('numbers' , 'count'));
+        return view('back.number.list', compact('numbers', 'count'));
     }
 
     public function create()
@@ -38,13 +42,13 @@ class NumberController extends Controller
 
     public function edit($id)
     {
-        $number = Number::find($id) ?? abort(403 , 'Nömrə Tapılmadı...');
-        return view('back.number.edit' , compact('number'));
+        $number = Number::find($id) ?? abort(403, 'Nömrə Tapılmadı...');
+        return view('back.number.edit', compact('number'));
     }
 
     public function update(NumberUpdate $request, $id)
     {
-        Number::find($id)->update(request()->post()) ?? abort(403 , 'Nömrə Tapılmadı...');
+        Number::find($id)->update(request()->post()) ?? abort(403, 'Nömrə Tapılmadı...');
         return redirect()->route('numbers.index')->withSuccess('Uğurla yeniləndi...');
     }
 
@@ -65,17 +69,25 @@ class NumberController extends Controller
 
 
         $file = $request->file('excel');
-        Excel::import(new NumbersImport, $file);
-        // (new NumbersImport)->import($file);
-        return redirect()->route('numbers.index')->withSuccess('Fayl uğurla yükləndi...');
 
+
+        $fileName = $request->file('excel')->getClientOriginalName();
+        $file2 = Str::substr($fileName, -4);
+        if ($file2 !== 'xlsx') {
+            return redirect()->back()->withErrors('Fayl formatı xlsx olmalıdır...');
+        } else {
+            Excel::import(new NumbersImport, $file);
+
+            // (new NumbersImport)->import($file);
+            return redirect()->route('numbers.index')->withSuccess('Fayl uğurla yükləndi...');
+        }
     }
 
 
-    public function allDelete(Request $request)
+    public function allDelete(ImportRequest $request)
     {
         $ids = $request->ids;
-        Number::whereIn('id' , $ids)->delete();
+        Number::whereIn('id', $ids)->delete();
         $message = array('message' => 'Uğurla Silindi...');
         return response()->json($message);
     }
@@ -101,52 +113,42 @@ class NumberController extends Controller
         $count   = count(Number::all());
         $numbers = Number::orderByDesc('id');
 
-        if (!empty($request->get('price'))) 
-        {
+        if (!empty($request->get('price'))) {
             $numbers = $numbers->where('price', $request->get('price'));
         }
 
-        if (!empty($request->get('prefix'))) 
-        {
+        if (!empty($request->get('prefix'))) {
             $numbers = $numbers->where('prefix', $request->get('prefix'));
         }
 
-        if (!empty($request->get('num1'))) 
-        {
+        if (!empty($request->get('num1'))) {
             $numbers = $numbers->where('num1', $request->get('num1'));
         }
 
-        if (!empty($request->get('num2'))) 
-        {
+        if (!empty($request->get('num2'))) {
             $numbers = $numbers->where('num2', $request->get('num2'));
         }
 
-        if (!empty($request->get('num3'))) 
-        {
+        if (!empty($request->get('num3'))) {
             $numbers = $numbers->where('num3', $request->get('num3'));
         }
 
-        if (!empty($request->get('num4'))) 
-        {
+        if (!empty($request->get('num4'))) {
             $numbers = $numbers->where('num4', $request->get('num4'));
         }
 
-        if (!empty($request->get('num5'))) 
-        {
+        if (!empty($request->get('num5'))) {
             $numbers = $numbers->where('num5', $request->get('num5'));
         }
 
-        if (!empty($request->get('num6'))) 
-        {
+        if (!empty($request->get('num6'))) {
             $numbers = $numbers->where('num6', $request->get('num6'));
         }
-        if (!empty($request->get('num7'))) 
-        {
-           $numbers = $numbers->where('num7', $request->get('num7'));
+        if (!empty($request->get('num7'))) {
+            $numbers = $numbers->where('num7', $request->get('num7'));
         }
 
         $numbers = $numbers->paginate(20);
-        return view('back.number.list' , compact('numbers' , 'count'));
-
+        return view('back.number.list', compact('numbers', 'count'));
     }
 }
--- app/Imports/NumbersImport.php
+++ app/Imports/NumbersImport.php
@@ -3,11 +3,9 @@
 namespace App\Imports;
 
 use Maatwebsite\Excel\Concerns\ToModel;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 use App\Models\Number;
-
-
+use Illuminate\Http\Request;
 
 
 class NumbersImport implements ToModel 
@@ -15,13 +13,6 @@ class NumbersImport implements ToModel
     public function model(array $row)
     {
 
-        // if(empty($row[1])) {
-        //     return Arr::prepend($row[1], 10);
-        // }
-
-        // if(empty($row[1])) {
-        //     return $row[1]->push(10);
-        // }
 
         return new Number([
             'prefix'=> str::substr($row[0] , 0 , 2),
@@ -32,10 +23,10 @@ class NumbersImport implements ToModel
             'num5'  => str::substr($row[0] , -3 , 1),
             'num6'  => str::substr($row[0] , -2 , 1),
             'num7'  => str::substr($row[0] , -1),
-            'price'   => $row[1],
-            'contact' => $row[2],
-            'seller'  => $row[3],
-            'operator'=> $row[4]
+            'price'   => $row[1] ?? 10,
+            'contact' => $row[2] ?? null,
+            'seller'  => $row[3] ?? null,
+            'operator'=> $row[4] ?? null
 
         ]);
     }
